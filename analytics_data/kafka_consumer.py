43f5ee75d61354dcb1beca5ecb7d803d2bc09ab0
GArthTex66 <gabrielarthur@unifei.edu.br>
2024-02-02 23:11:05 -0300

    add possible new data analysis service

---
+++ analytics_data/kafka_consumer.py
@@ -0,0 +1,20 @@
+from confluent_kafka import Consumer
+
+consumer = Consumer({'bootstrap.servers':'localhost:9092','group.id':'analytics-posts-group','auto.offset.reset':'earliest'})
+
+print('Kafka Consumer has been initiated...')
+
+print('Available topics to consume: ', consumer.list_topics().topics)
+consumer.subscribe(['car-post-topic'])
+
+def main():
+    while True:
+        msg = consumer.poll(1.0) #timeout
+        if msg is None:
+            continue
+        if msg.error():
+            print('Error: {}'.format(msg.error()))
+            continue
+        data = msg.value().decode('utf-8')
+        print(data)
+    consumer.close()
